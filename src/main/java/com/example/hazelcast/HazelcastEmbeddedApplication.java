2b140f125a126bd8493f29e4e374fea7538a5fdf
Deepa Ann John <deepa.ann.john1@gmail.com>
2024-11-24 18:01:11 +0530

    init

---
+++ src/main/java/com/example/hazelcast/HazelcastEmbeddedApplication.java
@@ -0,0 +1,13 @@
+
+package com.example.hazelcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastEmbeddedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HazelcastEmbeddedApplication.class, args);
+    }
+}
